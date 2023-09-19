c51da64061bdcd6fa8948cc17a716319985e8ea4
David <david.levchenko@gmail.com>
2023-09-19 23:30:45 +0300

    firsr commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const use='Ivan'
+alert(`hi, ${use}`)
+let number=4.6;
+console.log(4/0);
+console.log('string'*34);
+const persone='Alex';
+const bool=true;
+// console.log(something);
+let i;
+console.log(i);
+
+
+const obj = {
+    name: 'John',
+    age:25,
+    isMarried:false
+};
+// console.log(obj.name);
+console.log(obj['name']);
+
+
+let arr=['plum.pbg', 'orange.jpg',6,{ name: 'John',
+age:25,
+isMarried:false},'apple.bmp']
+console.log(arr[3]);
+
+// const array=[1,2,3];
+// const obj1={a:1,b:2};
+// console.log(array[0]);
+// console.log(obj1.a);
+const obj1={
+    'Anna':500,
+    'Alice':600
+};
+console.log(obj1.Anna);
+// const arrObj={
+//     0:a,
+//     1:b,
+//     2:c
+// }
+// console.log(arrObj[1]);
+
+// const array=[a,b,c];
+// console.log(array[0]);
+
+const person={
+    name:'Anna',
+    lastName:'Levchenko'
+};
+// console.log(person.name);
+console.log(person["name"]);
+
+person.number=1234;
+console.log(person.number);
+
+let array=[1,2,3]
+console.log(array[0]);
+
+let user={
+    0:'b',
+    abc:{esr:[1,2,3,{}]},
+    hyg:[],
+    1:'c'
+}
+console.log(user[0]);
+
+const b ='b'
+user.c=b
+console.log(user.c);
+
+
+
+// alert('GHello');
+
+// const result=confirm('Are your here?')
+// console.log(result);
+
+// const answer=+prompt("Вам есть 18", '18')
+// console.log(typeof (answer));
+// console.log( (answer+5));
+
+const answers=[];
+answers[0]=prompt('What is your name?', '')
+answers[1]=prompt('What is your lsat name?', '')
+answers[2]=prompt('What is your age?', '')
+
+//snake_case
+//UPER_SNAKE_CASE  const
+const COLOR_RED='#F00'
+const _apiKey = 'Dont tatch'
+//Kebab-case
+//PaskalCase
+
+
+const category = 'toys'
+
+// console.log('https://someurl.com/'+category);
+console.log(`https://someurl.com/${category}`);
+
